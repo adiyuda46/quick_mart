11b6a80c0ea232ec074ed86a42b1fcbb4eead934
adiyuda <adiyudapratama4@gmail.com>
2023-10-08 22:26:17 +0700

    [add] widget search

---
+++ lib/ui/widget/search.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatefulWidget {
+  const Search({super.key});
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search...',
+          border: InputBorder.none
+        ),
+      ),
+    );
+  }
+}
